37c1ae80f63984c83c91e4a473fdbbc326502580
fsh <1012960050@qq.com>
2018-04-06 22:41:15 +0800

    首页侧边栏和顶部固定完成，添加NProgress插件

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -2,80 +2,44 @@
  * Created by Administrator on 2018/4/6 0006.
  */
 $(function () {
-  //表单验证
-  $('#form').bootstrapValidator({
-    //默认图标
-    feedbackIcons: {
-      valid: 'glyphicon glyphicon-ok',
-      invalid: 'glyphicon glyphicon-remove',
-      validating: 'glyphicon glyphicon-refresh'
-    },
-    //校验字段
-    fields: {
-      username: {
-        validators: {
-          notEmpty: {
-            message: '用户名不能为空'
-          },
-          stringLength: {
-            mix: 6,
-            max: 10,
-            message: '用户名长度必须为6-10位'
-          },
-          callback: {
-            message: "用户名不存在"
-          }
-        }
-      },
-      password: {
-        validators:{
-          notEmpty:{
-            message:'密码不能为空'
-          },
-          stringLength: {
-            mix: 6,
-            max: 10,
-            message: '密码长度必须为6-10位'
-          },
-          callback:{
-            message:'密码错误'
-          }
-        }
-      }
-    }
+  //禁用小圆环
+  NProgress.configure({ showSpinner: false });
+  //进度条插件
+  $(document).ajaxStart(function () {
+    NProgress.start();
   });
-//注册表单验证成功事件
-  $('#form').on('success.form.bv',function (e) {
-    e.preventDefault();
+  $(document).ajaxStop(function() {
+    // 模拟网络延迟
+    setTimeout(function() {
+      NProgress.done();
+    }, 500);
+  });
+  //二级分类切换
+  $('.category').click(function () {
+    $(this).next().stop().slideToggle();
+  });
+  //左侧导航隐藏与显示
+  $('.icon-menu').on('click',function () {
+    $('.aside-nav').toggleClass('hide-btn');
+    $('.it-main').toggleClass('hide-btn');
+    $('.it-topbar').toggleClass('hide-btn');
+  });
+  //点击退出弹出模态框
+  $('.icon-logout').click(function() {
+    // 让模态框显示
+    $('#logoutModal').modal("show");
+  });
+
+  //点击退出点击事件
+  $('#logoutBtn').click(function () {
     $.ajax({
-      type:'post',
-      url:'/employee/employeeLogin',
-      data:$('#form').serialize(),
-      success: function (info) {
+      url:'/employee/employeeLogout',
+      dataType: "json",
+      success:function (info) {
         if(info.success){
-          location.href = "index.html";
-        }
-        if(info.error == 1000){
-          $('#form').data('bootstrapValidator').updateStatus('username','INVALID','callback');
-        }
-        if(info.error == 1001){
-          $('#form').data('bootstrapValidator').updateStatus('password','INVALID','callback');
+          location.href = "login.html";
         }
       }
     });
   });
-
-
-
-  //重置表单验证
-
-  $("[type='reset']").click(function () {
-    $('#form').data('bootstrapValidator').resetForm();
-  });
-
-
-
-
-
-
 });
